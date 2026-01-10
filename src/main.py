d5d80ab664ab73ee60ace9e4df1777c5e927ad72
tb.noah.clipping <toolbox.noah.clipping@gmail.com>
2026-01-10 12:27:07 +0900

    ベース環境

---
+++ src/main.py
@@ -0,0 +1,13 @@
+def function_example(arg1: str, arg2: int) -> None:
+    """Example function that takes a string and an integer as arguments"""
+    print(f'arg1: {arg1}, arg2: {arg2}')
+
+
+def main() -> None:
+    """Main function"""
+    print('Hello, World!')
+    function_example('hello world', 42)
+
+
+if __name__ == '__main__':
+    main()
